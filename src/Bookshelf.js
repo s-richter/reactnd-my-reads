de09f2514ee2b69adc63783df2c623c67fbb0519
Stephan Richter <stephan.richter@aon.at>
2017-08-23 17:10:01 +0200

    Create basic structure of React project

---
+++ src/Bookshelf.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Book from './Book'
+
+class Bookshelf extends React.Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default Bookshelf
