1fe140a159e8185deffee67396340881ac2ffbe0
Stephan Richter <stephan.richter@aon.at>
2017-08-24 13:51:14 +0200

    Display books fetched via BooksAPI.getall() instead of the hard-coded demo books

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -4,7 +4,18 @@ import Book from './Book'
 class Bookshelf extends React.Component {
     render() {
         return (
-            <div></div>
+            <div className="bookshelf">
+                <h2 className="bookshelf-title">{this.props.title}</h2>
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        {this.props.books.map((book) => (
+                            <li key={book.id}>
+                                <Book title={book.title} imageLinks={book.imageLinks} authors={book.authors} />                               
+                            </li>
+                        ))}
+                    </ol>
+                </div>
+            </div>
         )
     }
 }
