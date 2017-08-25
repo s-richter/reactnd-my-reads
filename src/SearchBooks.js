0769d7485b3ef514624dc721a703fd7441eb5f90
Stephan Richter <stephan.richter@aon.at>
2017-08-25 12:54:51 +0200

    feature: Add basic search functionality and log the result to the console

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,7 +1,67 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
 
 class SearchBooks extends React.Component {
+    static searchTermns = ['Android', 'Art', 'Artificial Intelligence', 'Astronomy',
+        'Austen', 'Baseball', 'Basketball', 'Bhagat', 'Biography', 'Brief',
+        'Business', 'Camus', 'Cervantes', 'Christie', 'Classics', 'Comics', 'Cook',
+        'Cricket', 'Cycling', 'Desai', 'Design', 'Development', 'Digital Marketing',
+        'Drama', 'Drawing', 'Dumas', 'Education', 'Everything', 'Fantasy', 'Film',
+        'Finance', 'First', 'Fitness', 'Football', 'Future', 'Games', 'Gandhi',
+        'Homer', 'Horror', 'Hugo', 'Ibsen', 'Journey', 'Kafka', 'King', 'Lahiri',
+        'Larsson', 'Learn', 'Literary Fiction', 'Make', 'Manage', 'Marquez',
+        'Money', 'Mystery', 'Negotiate', 'Painting', 'Philosophy', 'Photography',
+        'Poetry', 'Production', 'Programming', 'React', 'Redux', 'River', 'Robotics',
+        'Rowling', 'Satire', 'Science Fiction', 'Shakespeare', 'Singh', 'Swimming',
+        'Tale', 'Thrun', 'Time', 'Tolstoy', 'Travel', 'Ultimate', 'Virtual Reality',
+        'Web Development', 'iOS']
+
+    state = {
+        query: '',
+        matchingBooks: [],
+        result: '' // TODO: remove
+    }
+
+    updateQuery = (query) => {
+        console.log('---- query updated: ' + query + '----')
+        this.setState({ query: query })
+        if (query.length > 1) {
+            console.log('query.length: ' + query.length)
+            BooksAPI.search(query)
+                .then((result) => {
+                    console.log('result: ' + result)
+                    if (Array.isArray(result)) {
+                        console.log('result is an Array:')
+                        console.log(result.map((b) => b.title).join(', '))
+                        this.setState({
+                            matchingBooks: result
+                        })
+                    } else {
+                        console.log('result is NOT an Array:')
+                        this.setState({
+                            matchingBooks: []
+                        })
+                    }
+                    console.log('matchingBooks is now: ')
+                    console.log(this.state.matchingBooks.map((b) => b.title).join(', '))
+                    this.setState({
+                        result: this.state.matchingBooks.map((b) => b.title).join(', ')
+                    })
+                })
+                .catch((ex) => {
+                    console("exception: " + ex)
+                    this.setState({
+                        matchingBooks: []
+                    })
+                })
+        } else {
+            this.setState({
+                result: ''
+            })
+        }
+    }
+
     render() {
         return (
             <div className="search-books">
@@ -16,13 +76,18 @@ class SearchBooks extends React.Component {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                        <input type="text" placeholder="Search by title or author" />
-
+                        <input
+                            type="text"
+                            placeholder="Search by title or author"
+                            value={this.state.query}
+                            onChange={(event) => { this.updateQuery(event.target.value) }}
+                        />
                     </div>
                 </div>
                 <div className="search-books-results">
                     <ol className="books-grid"></ol>
                 </div>
+                <div>{this.state.result}</div>
             </div>
         )
     }
