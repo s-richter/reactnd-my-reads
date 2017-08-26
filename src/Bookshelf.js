ed9e7f54da7b69100e0fce95c03d1c10009113ec
Stephan Richter <stephan.richter@aon.at>
2017-08-26 16:30:04 +0200

    refactor: Make class component 'Bookshelf' a functional component (has no state anyway)

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,21 +1,19 @@
 import React from 'react'
 import BooksGrid from './BooksGrid'
 
-class Bookshelf extends React.Component {
-    render() {
-        return (
-            <div className="bookshelf">
-                <h2 className="bookshelf-title">{this.props.title}</h2>
-                <div className="bookshelf-books">
-                    <BooksGrid
-                        books={this.props.books}
-                        handleChangeShelf={(book, shelf) =>
-                            this.props.handleChangeShelf(book, shelf)}
-                    />
-                </div>
+function Bookshelf(props) {
+    return (
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">{props.title}</h2>
+            <div className="bookshelf-books">
+                <BooksGrid
+                    books={props.books}
+                    handleChangeShelf={(book, shelf) =>
+                        props.handleChangeShelf(book, shelf)}
+                />
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default Bookshelf
