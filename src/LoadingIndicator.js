3417a0e20a03c5b3545593f23a8cdb3a03f3caad
Stephan Richter <stephan.richter@aon.at>
2017-09-27 13:53:49 +0200

    refactor: change animated loading indicator

--- src/LoadingIndicator.js
+++ src/LoadingIndicator.js
@@ -3,10 +3,13 @@ import React from 'react'
 // A functional component indicating when the app is busy
 function LoadingIndicator(props) {
     return <div className="loading-indicator">
-        <div className="loading-dot"/>
-        <div className="loading-dot"/>
-        <div className="loading-dot"/>
-        <div className="loading-dot"/>
+        <div className='loading-letter'>l</div>
+        <div className='loading-letter'>o</div>
+        <div className='loading-letter'>a</div>
+        <div className='loading-letter'>d</div>
+        <div className='loading-letter'>i</div>
+        <div className='loading-letter'>n</div>
+        <div className='loading-letter'>g</div>
     </div>
 }
 
